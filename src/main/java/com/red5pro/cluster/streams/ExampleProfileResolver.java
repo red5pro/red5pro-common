7afd4989001faa921afc8a5a49ca2adbddbc1e3e
Andy--S <bowljoman@hotmail.com>
2018-02-21 14:23:07 -0800

    Updated for auto-config.

--- src/main/java/com/red5pro/cluster/streams/ExampleProfileResolver.java
+++ src/main/java/com/red5pro/cluster/streams/ExampleProfileResolver.java
@@ -1,12 +1,18 @@
 package com.red5pro.cluster.streams;
+
 /**
- * This example class parses the bitrate levels.
+ * This example class parses the variant index. It anticipates name_1, name_2, or name. 
+ * Lower non-zero indexes are higher quality. 
+ * Zero specifies one single quality available. An FMLE type GUI would use '_%i' added to the stream name. 
+ * Wirecast would enter specific names with '_1', '_2' added where '_1' is number one quality.
+ * When providing quality variant names, Red5Pro requires lower indexes to be higher quality.   
  * @author Andy Shaules
  *
  */
 public class ExampleProfileResolver implements ProfileLevelResolver {
-
 	
+	private String spliterator = "_";;
+
 	public ExampleProfileResolver(){
 		ProfileLevelResolver.LevelResolvers.add(this);
 	}
@@ -15,16 +21,21 @@ public class ExampleProfileResolver implements ProfileLevelResolver {
 	public int getLevel(String contect, String name) {
 		//Parse name to determine which bitrate level priority this stream is.
 		//lowest  name_3  
-		if(name.endsWith("_3"))
-		    return 3;
-		//middle  name_3  
-		if(name.endsWith("_2"))
-		    return 2;
-		//highest
-		if(name.endsWith("_1"))
-		    return 1;
-		//single bitrate
-		return 0;
+		String[] parts = name.split(spliterator) ;
+		// last part should be integer.
+		int level = 0;
+		try{
+			level = Integer.parseInt( parts[parts.length-1] );
+		
+		}catch(NumberFormatException nfe){			
+		}
+		return level;
+	}
+
+	@Override
+	public void setSpliteration(String pattern) {
+		this.spliterator  = pattern;
+		
 	}
 
 }
