6f64440721e79fbc4fc18438bbd158e83061899b
Andy--S <bowljoman@hotmail.com>
2018-02-22 09:58:09 -0800

    Cluster link state api added

--- src/main/java/com/red5pro/cluster/LinkStateListener.java
+++ src/main/java/com/red5pro/cluster/LinkStateListener.java
@@ -1,6 +1,10 @@
 package com.red5pro.cluster;
 
-
+/**
+ * Used to be notified of cluster link states. 
+ * @author Andy Shaules
+ *
+ */
 public interface LinkStateListener{
 	void originAck(String origin);		
 	void originClose(String host);
