a2d714efebeef7dbcc14daa3c926298e6950cede
Andy--S <bowljoman@hotmail.com>
2019-02-19 12:47:53 -0800

    formatted

--- src/main/java/com/red5pro/server/stream/mpegts/IConsumerStatistics.java
+++ src/main/java/com/red5pro/server/stream/mpegts/IConsumerStatistics.java
@@ -3,8 +3,8 @@ package com.red5pro.server.stream.mpegts;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public interface IConsumerStatistics {
-    static final CopyOnWriteArraySet<IConsumerStatistics> listeners = new CopyOnWriteArraySet<>();
-    
-    public void receiveStats(String remoteAddress,String path, String name, long bytes );
-    
+	static final CopyOnWriteArraySet<IConsumerStatistics> listeners = new CopyOnWriteArraySet<>();
+
+	public void receiveStats(String remoteAddress, String path, String name, long bytes);
+
 }
