ed5f3192ccf1340462c4de04dd95686321645dcd
Paul Gregoire <mondain@gmail.com>
2019-10-24 14:20:15 -0700

    Added updated setFormat method

--- src/main/java/javax/media/Buffer.java
+++ src/main/java/javax/media/Buffer.java
@@ -713,6 +713,11 @@ public class Buffer {
 		}
 	}
 
+    public void setFormat(Format format, RTPCodecEnum codec) {
+        this.format = format;
+        this.codec = codec;
+	}	
+	
 	/**
 	 * Returns whether or not this buffer has a format set and contains audio.
 	 * 
