7b8a010135cd473a583428c072aadbf494348629
Paul Gregoire <paul@infrared5.com>
2020-06-26 10:28:32 -0700

    Interfaces for mpegts update

---
+++ src/main/java/com/red5pro/server/stream/mpegts/ISegmentListener.java
@@ -0,0 +1,16 @@
+package com.red5pro.server.stream.mpegts;
+
+/**
+ * Callback interface for segment events.
+ * 
+ * @author Andy Shaules
+ * @author Paul Gregoire
+ *
+ */
+public interface ISegmentListener {
+
+	void onNewSegment(ITSPlaylist list);
+
+	void onWriteComplete(ITSStreamSegment segment);
+
+}
