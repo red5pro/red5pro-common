34ec13b8e9ed526cedff04ae1f3c9301c805e543
Andy--S <bowljoman@hotmail.com>
2020-12-21 10:43:57 -0800

    For externalizing group handler defs

---
+++ src/main/java/com/red5pro/group/ICoreProvider.java
@@ -0,0 +1,24 @@
+package com.red5pro.group;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+/**
+ * Provide logical handlers for Composition objects.
+ * 
+ * @author Andy
+ *
+ */
+public interface ICoreProvider {
+	/**
+	 * Application developers add handlers here.
+	 */
+	public static List<ICoreProvider> registry = new CopyOnWriteArrayList<>();
+	/**
+	 * Return the IGroupCore impl or null.
+	 * 
+	 * @param clazz
+	 * @return
+	 */
+	IGroupCore resolve(String clazz);
+
+}
