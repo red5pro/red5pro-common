3dde243982827717d3afc48f435817d7ed6ada37
Paul Gregoire <mondain@gmail.com>
2021-08-18 11:36:53 -0700

    Added secure id gen methods from goz

--- src/main/java/com/red5pro/util/IdGenerator.java
+++ src/main/java/com/red5pro/util/IdGenerator.java
@@ -1,10 +1,20 @@
 package com.red5pro.util;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.text.RandomStringGenerator;
 import org.bouncycastle.crypto.digests.SHA1Digest;
 import org.bouncycastle.crypto.prng.DigestRandomGenerator;
+import org.bouncycastle.util.encoders.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Id generator.
@@ -13,10 +23,28 @@ import org.bouncycastle.crypto.prng.DigestRandomGenerator;
  */
 public class IdGenerator {
 
+    private static Logger log = LoggerFactory.getLogger(IdGenerator.class);
+
     private static final DigestRandomGenerator random = new DigestRandomGenerator(new SHA1Digest());
 
     // random numeric string generator - thread safe and immutable
     public static RandomStringGenerator randomNumericStringGenerator = new RandomStringGenerator.Builder().withinRange('1', '9').build();
+
+    private static MessageDigest md;
+
+    static {
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            log.warn("Algorithm SHA256 not found, falling back to MD5");
+            // use simplified digest
+            try {
+                md = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e1) {
+                log.warn("Algorithm MD5 not found, falling back to plain hex");
+            }
+        }
+    }
 
     /**
      * Returns a cryptographically generated id of 128 bits.
@@ -72,6 +100,57 @@ public class IdGenerator {
      */
     public static final String generateNumericStringId(int length) {
         return randomNumericStringGenerator.generate(13);
+    }
+
+    /**
+     * Generate a UUID with source bytes from UUID.randomUUID().
+     * 
+     * @return hashed localized UUID
+     */
+    public static String generateSha256UUID() {
+        try {
+            String uuid = UUID.randomUUID().toString();
+            return (md != null) ? new DigestUtils(md).digestAsHex(uuid) : Hex.toHexString(uuid.getBytes());
+        } finally {
+            // md is not thread-safe so be aware of it!
+            if (md != null) {
+                // reset so we can use the digest
+                md.reset();
+            }
+        }
+    }
+
+    /**
+     * Generate a UUID with source bytes from a local MAC address and system time.
+     * 
+     * @return hashed localized UUID
+     */
+    public static String generateLocalizedUUID() {
+        try {
+            NetworkInterface network = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+            byte[] mac = network.getHardwareAddress();
+            if (log.isDebugEnabled()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < mac.length; i++) {
+                    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                }
+                log.debug("MAC {} bytes: {}", sb.toString(), Hex.toHexString(mac));
+            }
+            ByteBuffer uuid = ByteBuffer.allocate(mac.length + 8);
+            uuid.put(mac);
+            uuid.putLong(System.currentTimeMillis());
+            uuid.flip();
+            return (md != null) ? new DigestUtils(md).digestAsHex(uuid.array()) : Hex.toHexString(uuid.array());
+        } catch (Exception e) {
+            log.warn("Exception getting MAC address, falling back to random UUID");
+            return generateSha256UUID();
+        } finally {
+            // md is not thread-safe so be aware of it!
+            if (md != null) {
+                // reset so we can use the digest
+                md.reset();
+            }
+        }
     }
 
 }
