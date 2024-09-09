914e9bca8160077054d02a1c9a9ee6c92d273b22
nateroe <nate@nateroe.com>
2024-09-09 12:05:56 -0700

    container node comment [AUTO-374]

--- src/main/java/com/red5pro/mixer/nodes/IContainerNode.java
+++ src/main/java/com/red5pro/mixer/nodes/IContainerNode.java
@@ -2,6 +2,11 @@ package com.red5pro.mixer.nodes;
 
 import java.util.List;
 
+/**
+ * Contains other nodes (children)
+ * 
+ * @author Nate Roe
+ */
 public interface IContainerNode {
     public List<AbstractNode> getNodes();
 }
